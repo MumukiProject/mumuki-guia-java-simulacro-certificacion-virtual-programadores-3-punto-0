f9466b962ad1dc79976cf278e8c66c323b7583f0
agus@mumuki.org <agus@mumuki.org>
2018-05-20 20:24:33 -0400

    Mumuki Export on 2018-05-20 20:24:33 -0400

---
+++ 00003_Gestor de paquetes de proyectores/default.java
@@ -0,0 +1,18 @@
+class GestorDePaquetesDeProyectores {
+  private List<PaqueteProyectores> paquetes;
+  private String nombreEmpresa;
+  
+  public GestorDePaquetesDeProyectores(String nombreEmpresa) {
+    this.paquetes = new ArrayList<>();
+    this.nombreEmpresa = nombreEmpresa;
+  }
+  public void setNombreEmpresa(nombreEmpresa) {
+    this.nombreEmpresa = nombreEmpresa;
+  }
+  public String getNombreEmpresa() {
+    return this.nombreEmpresa;
+  }
+  
+  // Implementá acá tu solución
+  
+}
--- 00003_Gestor de paquetes de proyectores/description.md
+++ 00003_Gestor de paquetes de proyectores/description.md
@@ -1,4 +1,3 @@
-
 > Implemente el método `agregarPaquete` de la
 > clase `GestorDePaquetesDeProyectores`. El mismo 
 > deberá agregar un nuevo paquete en la lista
