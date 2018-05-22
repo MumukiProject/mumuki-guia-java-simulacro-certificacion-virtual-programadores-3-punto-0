d27c023e2f0863176147a3ea0c739a002cf524d2
agus@mumuki.org <agus@mumuki.org>
2018-05-22 11:49:05 -0400

    Mumuki Export on 2018-05-22 11:49:05 -0400

--- 00001_Implementar desde el diagrama de clases/test.java
+++ 00001_Implementar desde el diagrama de clases/test.java
@@ -1,4 +1,4 @@
-PaqueteProyectores pp = new PaqueteProyectores();
+PaqueteProyectores pp = new PaqueteProyectores(1, "Martin", "Mendoza", 2.0);
 
 @Test
 public void declara_cod_paquete_de_proyectores() {
