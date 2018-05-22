356fa699b75c46c090c1823878e24b8c05011ef7
agus@mumuki.org <agus@mumuki.org>
2018-05-22 15:55:16 -0400

    Mumuki Export on 2018-05-22 15:55:16 -0400

--- 00004_Buscando Paquete/test.java
+++ 00004_Buscando Paquete/test.java
@@ -1,4 +1,19 @@
+GestorDePaquetesDeProyectores gp;
+PaqueteProyectores pp = new PaqueteProyectores(2, "marta", "mendoza", 2);
+
+@Before
+public void before() {
+  gp = new GestorDePaquetesDeProyectores("Correo Argentino");  
+}
+
+
 @Test
-public void testDescriptionExample() {
-  Assert.assertTrue(true);
+public void el_gestor_no_tiene_paquetes() {
+  Assert.assertEquals(null, gp.buscarPaquete(1), 0.5);
+}
+
+@Test
+public void el_gestor_tiene_1_paquetes() {
+  gp.agregarPaquete(pp);
+  Assert.assertEquals(2, gp.getbuscarPaquete(2).getCodPaquete(), 0.5);
 }
