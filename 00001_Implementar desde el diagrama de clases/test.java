d52cd582c7f755b59525d4dafd9eff654d9a52f5
agus@mumuki.org <agus@mumuki.org>
2018-05-22 11:50:08 -0400

    Mumuki Export on 2018-05-22 11:50:08 -0400

--- 00001_Implementar desde el diagrama de clases/test.java
+++ 00001_Implementar desde el diagrama de clases/test.java
@@ -2,5 +2,5 @@ PaqueteProyectores pp = new PaqueteProyectores(1, "Martin", "Mendoza", 2);
 
 @Test
 public void declara_cod_paquete_de_proyectores() {
-  Assert.assertTrue(pp.getClass().getDeclaredFields("codPaqueteDeProyectores"));
+  Assert.assertTrue(pp.getClass().getDeclaredField("codPaqueteDeProyectores"));
 }
