bed219f68153fe54116c4b944ad39193f860795c
agus@mumuki.org <agus@mumuki.org>
2018-05-22 15:13:46 -0400

    Mumuki Export on 2018-05-22 15:13:46 -0400

--- 00002_Paquete de proyectores/test.java
+++ 00002_Paquete de proyectores/test.java
@@ -1,4 +1,24 @@
+PaqueteProyectores pp;
+
+@Before
+public void before() {
+  pp = new PaqueteProyectores(1, "Martina", "Mendoza", 2);  
+}
+
+
 @Test
-public void testDescriptionExample() {
-  Assert.assertTrue(true);
+public void el_paquete_esta_en_transito() {
+  pp.setEnTransito(true);
+  Assert.assertTrue(pp.getEnTransito())
+}
+
+@Test
+public void el_paquete_tiene_codigo_1() {
+  Assert.assertEquals(pp.getCodPaquete(), 1)
+}
+
+@Test
+public void el_paquete_tiene_codigo_5() {
+  pp.setCodPaquete(5);    
+  Assert.assertEquals(pp.getCodPaquete(), 5)
 }
